93c9dcedb951c11493ec2b9df41a9f03acd98b10
Mohsin <mdmohsinhaider6@gmail.com>
2025-02-14 21:23:53 +0530

    first voting survey commit

---
+++ src/main/java/com/mohsin/voting_survey/VotingSurveyApplication.java
@@ -0,0 +1,13 @@
+package com.mohsin.voting_survey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingSurveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingSurveyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mohsin/voting_survey/VotingSurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohsin.voting_survey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingSurveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
